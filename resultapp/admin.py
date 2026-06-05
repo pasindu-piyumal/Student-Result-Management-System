2f2c4216c8d20b115525bbc81d3099738635da40
pasindu-piyumal <22cds0423@ms.sab.ac.lk>
2026-06-05 22:55:43 +0530

    Register Class, Subject, SubjectCombination, and Student models in Django admin

---
+++ resultapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Class)
+admin.site.register(Subject)
+admin.site.register(SubjectCombination)
+admin.site.register(Student)
